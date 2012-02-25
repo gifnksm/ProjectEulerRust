46761c06bf17f935bf13a4b8eec3b52584ce671c
NAKASHIMA, Makoto <makoto.nksm+github@gmail.com>
2012-02-25 14:25:59 +0900

    Use `uint::div_ceil' instead of own defined `div_ceil'.

--- 0001-0050/prob0004.rs
+++ 0001-0050/prob0004.rs
@@ -11,13 +11,8 @@ fn to_palindromic(n: u64, dup_flag: bool) -> u64 {
     }
 }
 
-fn div_ceil(a: u64, b: u64) -> u64 {
-    let d = a / b;
-    ret if a % b != 0u64 { d + 1u64 } else { d };
-}
-
 fn dividable_pairs(num: u64, min: u64, max: u64) -> [(u64, u64)] {
-    let div = u64::max(div_ceil(num, max), min);
+    let div = u64::max(uint::div_ceil(num, max), min);
     let result = [];
     while div * div <= num {
         if num % div == 0u64 {
