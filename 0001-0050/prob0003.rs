03c9768691fd53435f521b70fd56003a7ab5cd08
NAKASHIMA, Makoto <makoto.nksm+github@gmail.com>
2012-02-25 13:18:15 +0900

    Eliminates unnecessary mutability.

--- 0001-0050/prob0003.rs
+++ 0001-0050/prob0003.rs
@@ -15,16 +15,16 @@ fn isqrt(n: u64) -> u64 {
     ret min;
 }
 
-fn gen_prime(primes: [mutable u64]) -> [mutable u64] {
+fn gen_prime(primes: [u64]) -> [u64] {
     let num = alt vec::last(primes) {
-      none       { ret [mutable 2u64] }
-      some(2u64) { ret primes + [mutable 3u64] }
+      none       { ret [2u64] }
+      some(2u64) { ret primes + [3u64] }
       some(x)    { x + 2u64 }
     };
     while true {
         for p in primes {
             if p * p > num {
-                ret primes + [mutable num];
+                ret primes + [num];
             }
             if num % p == 0u64 {
                 break;
@@ -36,7 +36,7 @@ fn gen_prime(primes: [mutable u64]) -> [mutable u64] {
 }
 
 fn main() {
-    let primes = gen_prime([mutable]);
+    let primes = gen_prime([]);
     let num = 600851475143u64;
     while num != 1u64 {
         check vec::is_not_empty(primes);
