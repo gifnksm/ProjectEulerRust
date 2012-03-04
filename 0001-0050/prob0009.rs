0a087519e892c5d82fa63234f2b7d80542c76e39
gifnksm <makoto.nksm+github@gmail.com>
2012-03-04 12:35:31 +0900

    Reduce extra check

--- 0001-0050/prob0009.rs
+++ 0001-0050/prob0009.rs
@@ -1,9 +1,24 @@
 use std;
 
+fn isqrt(n: u64) -> u64 {
+    let (min, max) = (0u64, n);
+    while min < max {
+        let mid = (min + max + 1u64) / 2u64;
+        if (mid * mid) == n {
+            ret mid;
+        } else if (mid * mid) >= n {
+            max = mid - 1u64;
+        } else {
+            min = mid;
+        }
+    }
+    ret min;
+}
+
 fn find_pyrhagorean(sum: u64) -> [(u64, u64, u64)] {
     let answer = [];
     uint::range(2u64, sum - 2u) { |c|
-        uint::range(1u64, (sum - c) / 2u) { |a|
+        uint::range(1u64, uint::min((sum - c) / 2u, isqrt(c*c / 2u))) { |a|
             let b = sum - c - a;
             if a * a + b * b == c * c {
                 answer += [(a, b, c)];
