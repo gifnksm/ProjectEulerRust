fedd8bfdd9ae374ca763986779c2f66efe1ae46e
gifnksm <makoto.nksm+github@gmail.com>
2012-03-04 12:24:29 +0900

    Delete definition of `isqrt'

--- 0001-0050/prob0009.rs
+++ 0001-0050/prob0009.rs
@@ -1,21 +1,5 @@
 use std;
 
-fn isqrt(n: u64) -> u64 {
-    let (min, max) = (0u64, n);
-    while min < max {
-        let mid = (min + max + 1u64) / 2u64;
-        if (mid * mid) == n {
-            ret mid;
-        } else if (mid * mid) >= n {
-            max = mid - 1u64;
-        } else {
-            min = mid;
-        }
-    }
-    ret min;
-}
-
-
 fn find_pyrhagorean(sum: u64) -> [(u64, u64, u64)] {
     let answer = [];
     uint::range(2u64, sum - 2u) { |c|
