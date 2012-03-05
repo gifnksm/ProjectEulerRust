f173a6a530361577b93f7bae59207bf3fa4fc974
gifnksm <makoto.nksm+github@gmail.com>
2012-03-05 23:57:26 +0900

    Add problem #14

---
+++ 0001-0050/prob0014.rs
@@ -0,0 +1,34 @@
+use std;
+
+impl map for std::map::map<uint, uint> {
+    fn get_at(n: uint) -> uint {
+        alt self.find(n) {
+          some(x) { ret x; }
+          none    {
+            let x = if n % 2u == 0u {
+                self.get_at(n / 2u) + 1u
+            } else {
+                self.get_at(3u * n + 1u) + 1u
+            };
+            self.insert(n, x);
+            ret x;
+          }
+        }
+        
+    }
+}
+
+fn main() {
+    let map = std::map::new_uint_hash();
+    map.insert(1u, 1u);
+    let max     = 1u;
+    let max_idx = 1u;
+    uint::range(2u, 1000000u) { |n|
+        let x = map.get_at(n);
+        if x > max {
+            max     = x;
+            max_idx = n;
+        }
+    };
+    std::io::println(#fmt("%u => %u", max_idx, max));
+}
