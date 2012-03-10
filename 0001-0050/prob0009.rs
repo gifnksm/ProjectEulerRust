1c5e18f1388e3234516310c1a42bd5e3b721fdcf
gifnksm <makoto.nksm+github@gmail.com>
2012-03-10 20:54:22 +0900

    Rename euler::uint -> euler::calc

--- 0001-0050/prob0009.rs
+++ 0001-0050/prob0009.rs
@@ -1,12 +1,11 @@
 use std;
 use euler;
-
-import euint = euler::uint;
+import calc = euler::calc;
 
 fn find_pyrhagorean(sum: u64) -> [(u64, u64, u64)] {
     let answer = [];
     uint::range(2u64, sum - 2u) { |c|
-        uint::range(1u64, uint::min((sum - c) / 2u, euint::isqrt(c*c / 2u))) { |a|
+        uint::range(1u64, uint::min((sum - c) / 2u, calc::isqrt(c*c / 2u))) { |a|
             let b = sum - c - a;
             if a * a + b * b == c * c {
                 answer += [(a, b, c)];
