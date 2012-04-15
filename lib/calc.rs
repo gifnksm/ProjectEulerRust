752ce8f222ef07139d4544e25c5ad55d59a116a8
gifnksm <makoto.nksm+github@gmail.com>
2012-04-15 22:48:24 +0900

    Add euler::calc::each_fib()

--- lib/calc.rs
+++ lib/calc.rs
@@ -13,3 +13,26 @@ fn isqrt(n: u64) -> u64 {
     ret min;
 }
 
+fn each_fib(f: fn(&&u64)->bool) {
+    let mut (prev, cur) = (0u64, 1u64);
+    loop {
+        if !f(cur) { break; }
+        let next = prev + cur;
+        prev = cur;
+        cur  = next;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_each_fib() {
+        let fib = [ 1u64, 1u64, 2u64, 3u64, 5u64, 8u64, 13u64, 21u64, 34u64, 55u64, 89u64, 144u64, 233u64 ];
+        let mut calc = [];
+        for each_fib {|f|
+            if f > fib.last() { break; }
+            calc += [ f ];
+        };
+        assert fib == calc;
+    }
+}
