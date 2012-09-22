ee8b42868e1d6c303c454c32afba3c6182e10d15
gifnksm <makoto.nksm+github@gmail.com>
2012-09-23 00:20:37 +0900

    Add problem #16

--- 0001-0050/prob0016.rs
+++ 0001-0050/prob0016.rs
@@ -1,11 +1,16 @@
+extern mod euler;
+
+use euler::extnum::{ one, from_uint };
+use euler::bigint::{ BigInt };
+
 fn main() {
-    let mut i = 1u64;
-    for uint::range(0u, 63u) |_n| {
-        let sum = vec::foldl(0u, do vec::filter_map(str::chars(#fmt("%u", i as uint))) |c| {
-            uint::from_str(str::from_char(c))
-        }, |s, n| s + n);
-        io::println(#fmt("%20u => %3u", i as uint, sum));
-        i *= 2u64;
+    let mut i = one::<BigInt>();
+    for uint::range(0, 1000) |_n| {
+        i *= from_uint(2);
     }
+    let sum = do str::byte_slice(i.to_str()) |buf| {
+        buf.map(|c| c - ('0' as u8)).foldl(0, |s, e| s + (e as uint))
+    };
+    io::println(fmt!("%u", sum));
 }
 
