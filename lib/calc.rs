27a515631742bf626a1ec6978fe40b61f05c288a
gifnksm <makoto.nksm+github@gmail.com>
2012-09-26 00:07:58 +0900

    Add euler::calc::{ factorial, histogram, num_of_permutations }

--- lib/calc.rs
+++ lib/calc.rs
@@ -1,3 +1,9 @@
+use dvec::{ DVec };
+use cmp::{ Ord, Eq };
+use to_bytes::{ IterBytes };
+use hash::{ Hash };
+use std::map::{ HashMap };
+
 pub fn each_fib(f: fn(uint)->bool) {
     let mut (prev, cur) = (0, 1);
     loop {
@@ -8,9 +14,27 @@ pub fn each_fib(f: fn(uint)->bool) {
     }
 }
 
-// pub fn num_of_permutation(n: uint) -> uint {
-    
-// }
+pub fn factorial(n: uint) -> uint {
+    let mut prod = 1;
+    for uint::range(1, n + 1) |n| { prod *= n; }
+    return prod;
+}
+
+fn histogram<T: Eq IterBytes Hash Const Copy>(v: &[T]) -> HashMap<T, uint> {
+    let map = HashMap::<T, uint>();
+    for v.each |k| {
+        let val = do map.find(*k).map_default(1) |v| { v + 1 };
+        map.insert(*k, val);
+    }
+    return map;
+}
+
+pub fn num_of_permutations<T: Eq IterBytes Hash Copy>(hist: HashMap<T, uint>) -> uint {
+    let mut sum = 0;
+    let mut div = 1;
+    for hist.each_value |cnt| { sum += cnt; div *= factorial(cnt); }
+    return factorial(sum) / div;
+}
 
 #[cfg(test)]
 mod tests {
@@ -23,5 +47,36 @@ mod tests {
             calc += [ f ];
         }
         assert fib == calc;
+    }
+
+    #[test]
+    fn test_factorial() {
+        assert factorial(0) == 1;
+        assert factorial(1) == 1;
+        assert factorial(2) == 2;
+        assert factorial(3) == 6;
+        assert factorial(10) == 3628800;
+    }
+
+    #[test]
+    fn test_histogram() {
+        fn check(inp: &[uint], result: &[(uint, uint)]) {
+            let mut vec = ~[];
+            for histogram(inp).each |k, v| { vec += [(k, v)]; }
+            let sorted = std::sort::merge_sort(|a, b| a.first() <= b.first(), vec);
+            assert result == sorted;
+        }
+        check(&[1, 2, 3], &[(1, 1), (2, 1), (3, 1)]);
+        check(&[1, 1, 1, 2, 2, 3, 3, 4], &[(1, 3), (2, 2), (3, 2), (4, 1)]);
+        check(&[1, 1, 1, 2, 2, 1, 1], &[(1, 5), (2, 2)]);
+        check(&[], &[]);
+    }
+
+    #[test]
+    fn test_num_of_permutasions() {
+        assert num_of_permutations(histogram::<uint>(&[])) == 1;
+        assert num_of_permutations(histogram(&[1, 2, 3])) == 6;
+        assert num_of_permutations(histogram(&[1, 1, 1, 2, 3])) == 20;
+        assert num_of_permutations(histogram(&[1, 1, 1, 2, 3, 1, 1])) == 42;
     }
 }
