f228bb2439ffc88d3ae935bacf9df5868a374fef
gifnksm <makoto.nksm+github@gmail.com>
2012-09-27 20:13:14 +0900

    Add problem #31

---
+++ 0001-0050/prob0031.rs
@@ -0,0 +1,15 @@
+fn count_ways(sum: uint, coins: &[uint]) -> uint {
+    if coins.len() == 1 { return 1 }
+
+    let mut ans = 0;
+    for uint::range(0, sum / coins[0] + 1) |n| {
+        let d = sum - n * coins[0];
+        ans += count_ways(d, vec::view(coins, 1, coins.len()));
+    }
+    return ans;
+}
+
+fn main() {
+    let coins = [ 200, 100, 50, 20, 10, 5, 2, 1 ];
+    io::println(fmt!("%u", count_ways(200, coins)));
+}
