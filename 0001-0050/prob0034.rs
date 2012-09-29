6225096f560ca69f35c05c232caf6917d8b5f34b
gifnksm <makoto.nksm+github@gmail.com>
2012-09-29 22:13:24 +0900

    Add problem#34

---
+++ 0001-0050/prob0034.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let mut facts = [ mut 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]/10;
+    for uint::range(1, facts.len()) |i| {
+        facts[i] = facts[i - 1] * i;
+    }
+
+    io::println(fmt!("%u", facts[9]));
+    let mut answer = 0;
+    for uint::range(0, facts[9].to_str().len() * facts[9]) |n| {
+        let mut itr = n;
+        let mut sum = 0;
+        while itr > 0 {
+            sum += facts[itr % 10];
+            itr /= 10;
+        }
+        if sum == n {
+            io::println(fmt!("%u => %u", n, sum));
+            answer += sum;
+        }
+    }
+
+    io::println(fmt!("answer: %u", answer - 1 - 2));
+}
