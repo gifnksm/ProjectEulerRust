d9ad69e4924e6c001ffcfc0c46a45412d50b4320
gifnksm <makoto.nksm+github@gmail.com>
2012-10-07 20:31:16 +0900

    Add problem #38

---
+++ 0001-0050/prob0038.rs
@@ -0,0 +1,17 @@
+extern mod std;
+use std::sort::{ quick_sort };
+
+extern mod euler;
+use euler::calc::{ permutate_num, num_to_digits };
+
+fn main() {
+    for permutate_num(~[9, 8, 7, 6, 5, 4, 3, 2, 1], 4, 0, 9999) |num, rest| {
+        let mut ds = num_to_digits(num * 2, 10);
+        quick_sort(|a, b| a >= b, ds);
+
+        if vec::eq(ds, rest) {
+            io::println(fmt!("%u%u", num, num * 2));
+            break;
+        }
+    }
+}
