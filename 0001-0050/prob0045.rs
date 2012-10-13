fea643fd0fa306354c379dec86d7d509b0163500
gifnksm <makoto.nksm+github@gmail.com>
2012-10-13 22:24:22 +0900

    Add problem #45

---
+++ 0001-0050/prob0045.rs
@@ -0,0 +1,49 @@
+fn triangle(i: uint) -> uint {
+    let n = i + 1;
+    return n * (n + 1) / 2;
+}
+
+fn pentagonal(i: uint) -> uint {
+    let n = i + 1;
+    return n * (3 * n - 1) / 2;
+}
+
+fn hexagonal(i: uint) -> uint {
+    let n = i + 1;
+    return n * (2 * n - 1);
+}
+
+fn main() {
+    let mut n = 40755 + 1;
+    let mut t_i = 0;
+    let mut p_i = 0;
+    let mut h_i = 0;
+
+    loop {
+        let mut t = triangle(t_i);
+        while t < n {
+            t_i += 1;
+            t = triangle(t_i);
+        }
+        if t > n { n = t; }
+
+        let mut p = pentagonal(p_i);
+        while p < n {
+            p_i += 1;
+            p = pentagonal(p_i);
+        }
+        if p > n { n = p; loop; }
+
+        let mut h = hexagonal(h_i);
+        while h < n {
+            h_i += 1;
+            h = hexagonal(h_i);
+        }
+        if h > n { n = h; loop; }
+
+        break;
+    }
+
+    io::println(fmt!("T[%u] = P[%u] = H[%u] = %u", t_i, p_i, h_i, triangle(t_i)));
+    io::println(fmt!("answer: %u", triangle(t_i)));
+}
