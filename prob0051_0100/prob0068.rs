9165930874475622d6ad2ebcce98cdd2cd69c910
gifnksm <makoto.nksm+github@gmail.com>
2013-04-01 22:18:10 +0900

    Add problem #68

---
+++ prob0051_0100/prob0068.rs
@@ -0,0 +1,28 @@
+use common::problem::{ Problem };
+
+pub static problem: Problem<'static> = Problem {
+    id: 68,
+    answer: "6531031914842725",
+    solver: solve
+};
+
+fn solve() -> ~str {
+    // solve by hand...
+    //
+    // outside: 6, 10, 9, 8, 7
+    // (1 + 2 + 3 + 4 + 5) * 2 +
+    // 6 + 7 + 8 + 9 + 10
+    // = 15 + 55 = 70
+    // 70 / 5 = 14
+    //
+    // 6, 5, X is max => 6, 5, 3
+    //
+    // 6, 5, 3
+    // 10, 3, 1
+    // 9, 1, 4
+    // 8, 4, 2
+    // 7, 2, 5
+
+    ~"6531031914842725"
+}
+
