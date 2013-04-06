0e65b83082094453e86a6374e99b2ce91a2dd8a9
gifnksm <makoto.nksm+github@gmail.com>
2013-04-06 22:40:18 +0900

    Add problem #71

---
+++ prob0051_0100/prob0071.rs
@@ -0,0 +1,21 @@
+use common::problem::{ Problem };
+
+pub static problem: Problem<'static> = Problem {
+    id: 71,
+    answer: "428570",
+    solver: solve
+};
+
+fn solve() -> ~str {
+    let limit = 1000000;
+    let mut max_n = 0;
+    let mut max_d = 1;
+    for uint::range_rev(limit, limit - 7) |d| {
+        let n = if 3 * d % 7 == 0 { 3 * d / 7 - 1 } else { 3 * d / 7 };
+        if n *max_d > max_n * d {
+            max_n = n;
+            max_d = d;
+        }
+    }
+    return max_n.to_str();
+}
