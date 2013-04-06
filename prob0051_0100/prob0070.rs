c454b36f8b9bf02d5524ce9bc7f8e0b1246f173e
gifnksm <makoto.nksm+github@gmail.com>
2013-04-06 22:02:03 +0900

    Add problem #70

---
+++ prob0051_0100/prob0070.rs
@@ -0,0 +1,46 @@
+use common::prime::{ Prime };
+use common::calc::{ digit_histogram };
+use common::problem::{ Problem };
+
+pub static problem: Problem<'static> = Problem {
+    id: 70,
+    answer: "8319823",
+    solver: solve
+};
+
+fn solve() -> ~str {
+    let limit = 10000000;
+
+    // n / phi(n) = 1 / \Pi_{k=1}^d (1 - 1/p_k)
+    // => p^k / phi(p^k) = p / phi(p)
+    // p is greater then n / phi(p) is less
+    // 
+    // phi(p) = p - 1 (if p is prime) => phi(p) is not permutation of p
+    // phi(p1 * p2) = (p1 - 1) * (p2 - 1)
+
+    let mut ps = Prime::new();
+    let mut min_n   = 0;
+    let mut min_n_phi = float::infinity;
+    for ps.each_borrow |p1, ps| {
+        if p1 * p1 > limit { break; }
+        for ps.each |p2| {
+            if p2 < p1 { loop; }
+
+            let n = p1 * p2;
+            if n > limit { break; }
+
+            let phi = (p1 - 1) * (p2 - 1);
+            let ds1 = digit_histogram(n);
+            let ds2 = digit_histogram(phi);
+            if ds1 != ds2 { loop; }
+
+            let n_phi = (n as float) / (phi as float);
+            if n_phi < min_n_phi {
+                min_n_phi = n_phi;
+                min_n     = n;
+            }
+        }
+    }
+
+    return min_n.to_str();
+}
