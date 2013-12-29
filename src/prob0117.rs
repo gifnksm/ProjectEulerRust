c78cc443be91169de4bdd51c05822ae2e2b0f42d
gifnksm <makoto.nksm@gmail.com>
2013-12-29 22:08:46 +0900

    Add problem #117

---
+++ src/prob0117.rs
@@ -0,0 +1,40 @@
+#[crate_type = "rlib"];
+
+use std::iter;
+use std::hashmap::HashMap;
+
+pub static EXPECTED_ANSWER: &'static str = "100808458960497";
+
+fn count(len: uint, map: &mut HashMap<uint, uint>) -> uint {
+    match map.find(&len) {
+        Some(&x) => return x,
+        None => {}
+    }
+
+    let mut sum = 0;
+    for i in iter::range_inclusive(0, len) { // most left block position
+        if len - i >= 2 { sum += count(len - i - 2, map); } // red
+        if len - i >= 3 { sum += count(len - i - 3, map); } // green
+        if len - i >= 4 { sum += count(len - i - 4, map); } // blue
+    }
+    sum += 1; // all black
+    map.insert(len, sum);
+    sum
+}
+
+pub fn solve() -> ~str {
+    let mut map = HashMap::new();
+    count(50, &mut map).to_str()
+}
+
+#[cfg(test)]
+mod test {
+    use super::count;
+    use std::hashmap::HashMap;
+
+    #[test]
+    fn couunt_test() {
+        let mut map = HashMap::new();
+        assert_eq!(15, count(5, &mut map));
+    }
+}
