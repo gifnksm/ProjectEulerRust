e68710e1078c07ffe80a8ef7e7e94128157cd656
gifnksm <makoto.nksm+github@gmail.com>
2014-01-05 00:10:05 +0900

    ユニットテストを追加

--- src/prob0057.rs
+++ src/prob0057.rs
@@ -39,8 +39,31 @@ impl Iterator<(BigUint, BigUint)> for Frac {
 }
 
 pub fn solve() -> ~str {
-    Frac::new().take(1000)
-        .map(|(n, d)| (n.to_str().len(), d.to_str().len()))
-        .count(|(n_len, d_len)| n_len > d_len)
+    Frac::new()
+        .take(1000)
+        .count(|(n, d)| n.to_str().len() > d.to_str().len())
         .to_str()
+}
+
+#[cfg(test)]
+mod test {
+    use extra::bigint::BigUint;
+    use super::Frac;
+
+    #[test]
+    fn frac() {
+        fn gen(n: uint, d: uint) -> (BigUint, BigUint) {
+            (FromPrimitive::from_uint(n).unwrap(),
+             FromPrimitive::from_uint(d).unwrap())
+        }
+        let mut it = Frac::new();
+        assert_eq!(Some(gen(3, 2)), it.next());
+        assert_eq!(Some(gen(7, 5)), it.next());
+        assert_eq!(Some(gen(17, 12)), it.next());
+        assert_eq!(Some(gen(41, 29)), it.next());
+        assert_eq!(Some(gen(99, 70)), it.next());
+        assert_eq!(Some(gen(239, 169)), it.next());
+        assert_eq!(Some(gen(577, 408)), it.next());
+        assert_eq!(Some(gen(1393, 985)), it.next());
+    }
 }
