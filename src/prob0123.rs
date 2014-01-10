42d5bc909a7772eb7cd570b65f4b033cfc5b6355
gifnksm <makoto.nksm+github@gmail.com>
2014-01-10 18:08:57 +0900

    Add problem #123

---
+++ src/prob0123.rs
@@ -0,0 +1,37 @@
+#[crate_type = "rlib"];
+
+extern mod math;
+
+use std::iter;
+use math::arith;
+use math::prime::Prime;
+
+pub static EXPECTED_ANSWER: &'static str = "21035";
+
+// from problem 120
+// f(n) := (p[n]-1)^n + (p[n]+1)^n
+//
+// if n is even:
+//   f(n) ≡ 1  f   mod p[n]^2
+// if n is odd:
+//   f(n) ≡ 2np[n] mod p[n]^2
+
+fn get_mod(n: uint, pn: uint) -> uint {
+    if n.is_even() {
+        1
+    } else {
+        (2 * n * pn) % (pn * pn)
+    }
+}
+
+pub fn solve() -> ~str {
+    let limit = arith::pow(10, 10);
+
+    let ps = Prime::new();
+    iter::count(1, 1u)
+        .zip(ps.iter())
+        .find(|&(n, pn)| get_mod(n, pn) > limit)
+        .unwrap()
+        .n0()
+        .to_str()
+}
