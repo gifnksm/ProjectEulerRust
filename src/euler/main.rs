43b482c477596351642ddba7221f3491688b95b2
gifnksm <makoto.nksm+github@gmail.com>
2014-01-11 04:14:20 +0900

    euler のリファクタリング

--- src/euler/main.rs
+++ src/euler/main.rs
@@ -1,151 +1,65 @@
 #[crate_id = "euler"];
 #[crate_type = "bin"];
 
-#[feature(managed_boxes)];
-
 extern mod extra;
 extern mod common;
 
-use std::{io, os};
-use std::iter::Range;
-use extra::{time, term};
-use extra::term::Terminal;
-use problem::Problem;
+use std::os;
+use std::iter;
+use problem::{Problem, Solution};
 
 mod problem;
 mod problem_list;
 
-static NSEC_PER_SEC: u64 = 1000000000;
-fn nanosec_to_str(nsec: u64) -> ~str {
-    return format!("{}.{:09}",
-         (nsec / NSEC_PER_SEC) as uint,
-         (nsec % NSEC_PER_SEC) as uint);
-}
-
-fn bench<T>(f: proc() -> T) -> (u64, T) {
-    let start_time = time::precise_time_ns();
-    let result     = f();
-    let end_time   = time::precise_time_ns();
-    return (end_time - start_time, result);
-}
-
-fn color_print(color: term::color::Color, s: &str) {
-    match Terminal::new(io::stdout()) {
-        Ok(ref mut term) => {
-            term.fg(color);
-            term.write(s.as_bytes());
-            term.reset();
-        }
-        Err(_) => { io::stdout().write(s.as_bytes()) }
-    }
-}
-
-fn print_result(name: &str, time: u64, comp_answer: &str, time_color: bool, correct: bool) {
-    let out = &mut io::stdout();
-    write!(out, "[");
-    if correct {
-        color_print(term::color::GREEN, "OK");
-    } else {
-        color_print(term::color::RED, "NG");
-    }
-    write!(out, "] {:5}", name);
-    if (time_color && time > 10 * NSEC_PER_SEC) {
-        color_print(term::color::RED, format!(" {:13}", nanosec_to_str(time)));
-    } else if time_color && time > NSEC_PER_SEC {
-        color_print(term::color::YELLOW, format!(" {:13}", nanosec_to_str(time)));
-    } else {
-        write!(out, " {:13}", nanosec_to_str(time));
-    }
-    writeln!(out, " {:20}", comp_answer);
-}
-
-struct ArgIterator<'a> {
-    args: &'a [~str],
-    idx: uint,
-    cur_range: Range<uint>,
-}
-
-impl<'a> ArgIterator<'a> {
-    pub fn new<'a>(args: &'a [~str]) -> ArgIterator<'a> {
-        let mut it = ArgIterator { args: args, idx: 0, cur_range: range(0u, 0) };
-        it.update_range();
-        return it;
-    }
-
-    fn update_range(&mut self) {
-        self.cur_range = range(0u, 0);
-        if self.idx >= self.args.len() { return; }
-
-        if !self.args[self.idx].contains_char('-') {
-            let n = from_str::<uint>(self.args[self.idx]);
-            for &n in n.iter() {
-                self.cur_range = range(n, n + 1);
-            }
-            return;
-        }
-
-        let mut ns = ~[];
-        for ss in self.args[self.idx].split('-') {
-            match from_str::<uint>(ss) {
-                Some(n) => { ns.push(n); }
-                None    => { return; }
-            }
-        }
-        if ns.len() > 2 { return; }
-        self.cur_range = range(ns[0], ns[1] + 1);
-    }
-}
-
-impl<'a> Iterator<uint> for ArgIterator<'a> {
-    fn next(&mut self) -> Option<uint> {
-        loop {
-            if self.idx >= self.args.len() { return None; }
-            match self.cur_range.next() {
-                Some(n) => { return Some(n); }
-                None    => {
-                    self.idx += 1;
-                    self.update_range();
-                }
-            }
-        }
-    }
-}
-
-fn solve_all<T: Iterator<&'static Problem<'static>>>(mut it: T) {
+fn solve_all<T: Iterator<&'static Problem<'static>>>(mut it: T) -> bool {
     let mut total_time  = 0;
     let mut solve_cnt   = 0;
     let mut all_correct = true;
 
     for p in it {
-        let (time, answer) = do bench { (p.solve)() };
-        let correct = p.answer == answer;
-        print_result(p.id.to_str(), time, answer, true, correct);
+        let sol = p.solve();
+        sol.print(true);
 
-        total_time += time;
+        total_time += sol.time();
         solve_cnt  += 1;
-        all_correct &= correct;
+        all_correct &= sol.is_correct();
     }
 
     if solve_cnt > 1 {
-        print_result("AVG", total_time / solve_cnt, "", true, all_correct);
-        print_result("TOTAL", total_time, "", false, all_correct);
+        let avg_time = total_time / solve_cnt;
+        Solution::new(~"AVG",   all_correct, ~"", avg_time).print(true);
+        Solution::new(~"TOTAL", all_correct, ~"", total_time).print(false);
     }
 
-    if !all_correct {
-        os::set_exit_status(1);
+    all_correct
+}
+
+fn parse_range(s: &str) -> Option<(uint, uint)> {
+    if !s.contains_char('-') {
+        from_str::<uint>(s).map(|n| (n, n))
+    } else {
+        let mut ss = s.splitn('-', 1);
+        let opt_a = ss.next().and_then(|sa| from_str(sa));
+        let opt_b = ss.next().and_then(|sb| from_str(sb));
+        opt_a.and_then(|a| opt_b.map(|b| (a, b)))
     }
 }
 
 fn main() {
-    let args = os::args();
-    let args = args.tail();
+    let mut args = os::args();
+    args.shift();
 
-    if args.is_empty() {
-        solve_all(range(0, problem_list::PROBLEMS.len())
-                  .map(|i| problem_list::PROBLEMS[i]))
+    let all_correct = if args.is_empty() {
+        let it = problem_list::PROBLEMS.iter().map(|x| *x);
+        solve_all(it)
     } else {
-        solve_all(ArgIterator::new(args)
-                  .filter_map(|n| problem_list::PROBLEMS.bsearch(|&p| p.id.cmp(&n)))
-                  .map(|i| problem_list::PROBLEMS[i]));
+        let it = args.iter()
+            .filter_map(|s| parse_range(*s))
+            .flat_map(|(a, b)| iter::range_inclusive(a, b))
+            .filter_map(|id| problem_list::PROBLEMS.bsearch(|p| p.id.cmp(&id)))
+            .map(|i| problem_list::PROBLEMS[i]);
+        solve_all(it)
     };
+
+    if !all_correct { os::set_exit_status(1) }
 }
--- src/euler/problem.rs
+++ src/euler/problem.rs
@@ -1,5 +1,106 @@
+use std::io;
+use extra::{term, time};
+use extra::term::Terminal;
+use extra::term::color::Color;
+
+static WARN_COLOR_NSEC:  u64 = 1  * NSEC_PER_SEC;
+static ERROR_COLOR_NSEC: u64 = 10 * NSEC_PER_SEC;
+static NSEC_PER_SEC:     u64 = 1000000000;
+
+static COLOR_OK:   Color = term::color::GREEN;
+static COLOR_NG:   Color = term::color::RED;
+static COLOR_WARN: Color = term::color::YELLOW;
+
 pub struct Problem<'a> {
     id: uint,
     answer: &'a str,
     solve: extern fn() -> ~str
+}
+
+impl<'a> Problem<'a> {
+    pub fn solve(&self) -> Solution {
+        let (time, answer) = bench(|| (self.solve)());
+        Solution {
+            name: self.id.to_str(),
+            is_correct: answer.equiv(&self.answer),
+            answer: answer,
+            time: time
+        }
+    }
+}
+
+pub struct Solution {
+    priv name: ~str,
+    priv is_correct: bool,
+    priv answer: ~str,
+    priv time: u64
+}
+
+impl Solution {
+    pub fn new(name: ~str, is_correct: bool, answer: ~str, time: u64) -> Solution {
+        Solution {
+            name: name,
+            is_correct: is_correct,
+            answer: answer,
+            time: time
+        }
+    }
+
+    #[inline] pub fn is_correct(&self) -> bool { self.is_correct }
+    #[inline] pub fn time(&self) -> u64 { self.time }
+
+    pub fn print(&self, enable_time_color: bool) {
+        fn ok(s: ~str) -> (Option<Color>, ~str) { (Some(COLOR_OK), s) }
+        fn ng(s: ~str) -> (Option<Color>, ~str) { (Some(COLOR_NG), s) }
+        fn warn(s: ~str) -> (Option<Color>, ~str) { (Some(COLOR_WARN), s) }
+        fn normal(s: ~str) -> (Option<Color>, ~str) { (None, s) }
+
+        let result_mark = if self.is_correct { ok(~"OK") } else { ng(~"NG") };
+        let time_str = format!(" {:13}", nanosec_to_str(self.time));
+        let time_mark = if !enable_time_color || self.time < WARN_COLOR_NSEC {
+            normal(time_str)
+        } else if self.time < ERROR_COLOR_NSEC {
+            warn(time_str)
+        } else {
+            ng(time_str)
+        };
+
+        print_items(&[
+                normal(~"["), result_mark, normal(~"]"),
+                normal(format!(" {:5}", self.name)),
+                time_mark,
+                normal(format!(" {:20}", self.answer)),
+                normal(~"\n")
+                ]);
+    }
+}
+
+fn nanosec_to_str(nsec: u64) -> ~str {
+    return format!("{}.{:09}",
+         (nsec / NSEC_PER_SEC) as uint,
+         (nsec % NSEC_PER_SEC) as uint);
+}
+
+fn bench<T>(f: || -> T) -> (u64, T) {
+    let start_time = time::precise_time_ns();
+    let result     = f();
+    let end_time   = time::precise_time_ns();
+    return (end_time - start_time, result);
+}
+
+fn print_items(items: &[(Option<Color>, ~str)]) {
+    match Terminal::new(io::stdout()) {
+        Err(_) => {
+            let mut out = io::stdout();
+            for &(_, ref s) in items.iter() { out.write_str(*s); }
+        },
+        Ok(mut t) => {
+            for &(c, ref s) in items.iter() {
+                match c {
+                    Some(c) => { t.fg(c); t.write_str(*s); t.reset() }
+                    None    => { t.write_str(*s); }
+                }
+            }
+        }
+    }
 }
