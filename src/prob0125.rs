36951a52dfb4ab18be7a3519ab32f44d9f397dc6
gifnksm <makoto.nksm+github@gmail.com>
2014-01-12 02:13:14 +0900

    Add problem #125

---
+++ src/prob0125.rs
@@ -0,0 +1,46 @@
+#[crate_type = "rlib"];
+
+extern mod math;
+
+use std::iter;
+use std::hashmap::HashSet;
+use math::{arith, numconv};
+
+pub static EXPECTED_ANSWER: &'static str = "2906969179";
+
+fn palindromic_sum_set(limit: uint) -> HashSet<uint> {
+    let mut set = HashSet::new();
+    let mut sq_sums: ~[uint] = ~[];
+
+    let mut it = iter::count(1u, 1)
+        .map(|n| n * n)
+        .take_while(|&pow| pow < limit);
+
+    for pow in it {
+        for j in range(0, sq_sums.len()).invert() {
+            let s = sq_sums[j] + pow;
+            if s >= limit { break; }
+
+            if numconv::is_palindromic(s, 10) { set.insert(s); }
+            sq_sums[j] = s;
+        }
+        sq_sums.push(pow);
+    }
+
+    set
+}
+
+pub fn solve() -> ~str {
+    let limit = arith::pow(10, 8);
+    let set = palindromic_sum_set(limit);
+    set.iter().fold(0, |x, &y| x + y).to_str()
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn palindromic_sum_below_1000() {
+        let set = super::palindromic_sum_set(1000);
+        assert_eq!(4164, set.iter().fold(0, |x, &y| x + y));
+    }
+}
