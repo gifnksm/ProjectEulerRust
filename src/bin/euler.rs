daa9254e006bf907ba8b27c01865b8502a287173
gifnksm <makoto.nksm+github@gmail.com>
2015-02-28 07:34:36 +0900

    update to rustc 1.0.0-nightly (b47aebe3f 2015-02-26) (built 2015-02-27)

--- src/bin/euler.rs
+++ src/bin/euler.rs
@@ -2,7 +2,7 @@
         unused, unused_extern_crates, unused_import_braces,
         unused_qualifications, unused_results, unused_typecasts)]
 
-#![feature(env, old_io, old_path)]
+#![feature(env, io, old_io, os, path, process, std_misc)]
 
 extern crate glob;
 extern crate "rustc-serialize" as rustc_serialize;
@@ -12,10 +12,12 @@ extern crate common;
 use std::borrow::{Cow, IntoCow};
 use std::env;
 use std::error::FromError;
-use std::old_io as io;
-use std::old_io::{Command, MemReader};
-use std::old_io::process::ExitStatus;
+use std::io;
+use std::old_io;
+use std::path::Path;
+use std::process::Command;
 use std::str;
+use std::os::unix::ExitStatusExt;
 use glob::Paths;
 use rustc_serialize::Decodable;
 use rustc_serialize::json::{self, Json};
@@ -28,93 +30,90 @@ type ProgramResult<T> = Result<T, ProgramError>;
 type OutputPair<'a> = (Option<Color>, Cow<'a, str>);
 
 #[derive(Debug)]
-enum ProgramErrorKind {
-    IoError(io::IoError),
-    JsonSyntaxError(json::ErrorCode, usize, usize),
+enum ProgramError {
+    IoError(io::Error),
+    OldIoError(old_io::IoError),
+    JsonParserError(json::ParserError),
     JsonDecoderError(json::DecoderError),
-    Unknown
-}
-
-#[derive(Debug)]
-struct ProgramError {
-    kind: ProgramErrorKind,
-    message: Cow<'static, str>
+    Unknown(Cow<'static, str>)
 }
 
 impl ProgramError {
-    pub fn new<T: IntoCow<'static, str>>(msg: T, kind: ProgramErrorKind) -> ProgramError {
-        ProgramError {
-            kind: kind,
-            message: msg.into_cow()
-        }
+    fn unknown<T: IntoCow<'static, str>>(msg: T) -> ProgramError {
+        ProgramError::Unknown(msg.into_cow())
     }
 }
 
-impl FromError<io::IoError> for ProgramError {
-    fn from_error(err: io::IoError) -> ProgramError {
-        ProgramError::new(err.desc.into_cow(), ProgramErrorKind::IoError(err))
+impl FromError<io::Error> for ProgramError {
+    fn from_error(err: io::Error) -> ProgramError {
+        ProgramError::IoError(err)
+    }
+}
+
+impl FromError<old_io::IoError> for ProgramError {
+    fn from_error(err: old_io::IoError) -> ProgramError {
+        ProgramError::OldIoError(err)
     }
 }
 
 impl FromError<json::ParserError> for ProgramError {
     fn from_error(err: json::ParserError) -> ProgramError {
-        match err {
-            json::ParserError::SyntaxError(code, line, col) => {
-                ProgramError::new(format!("{}:{}:{}", line, col, json::error_str(code)),
-                                  ProgramErrorKind::JsonSyntaxError(code, line, col))
-            }
-            json::ParserError::IoError(kind, desc) => {
-                FromError::from_error(io::IoError {kind: kind, desc: desc, detail: None })
-            }
-        }
+        ProgramError::JsonParserError(err)
     }
 }
 
 impl FromError<json::DecoderError> for ProgramError {
     fn from_error(err: json::DecoderError) -> ProgramError {
-        ProgramError::new(format!("{:?}", err), ProgramErrorKind::JsonDecoderError(err))
+        ProgramError::JsonDecoderError(err)
     }
 }
 
-fn problem_paths(dir_path: Path) -> ProgramResult<Paths> {
+fn problem_paths(dir_path: &Path) -> ProgramResult<Paths> {
     let pat = dir_path.join(PROBLEM_EXE_PAT);
-    match pat.as_str() {
+    match pat.to_str() {
         Some(x) => Ok(glob::glob(x).unwrap()),
-        None    => Err(ProgramError::new("path contains non-utf8 character", ProgramErrorKind::Unknown))
+        None    => Err(ProgramError::unknown("path contains non-utf8 character"))
     }
 }
 
 fn run_problem(path: &Path) -> ProgramResult<SolverResult<String>> {
     let proc_out = try!(Command::new(path).arg("--json").output());
 
-    if !proc_out.error.is_empty() {
-        let _ = match str::from_utf8(&proc_out.error) {
-            Ok(s)  => writeln!(&mut io::stderr(), "{}", s.trim()),
-            Err(e) => writeln!(&mut io::stderr(), "{:?}: {}", proc_out.error, e)
+    if !proc_out.stderr.is_empty() {
+        let _ = match str::from_utf8(&proc_out.stderr) {
+            Ok(s)  => writeln!(&mut old_io::stderr(), "{}", s.trim()),
+            Err(e) => writeln!(&mut old_io::stderr(), "{:?}: {}", proc_out.stderr, e)
         };
     }
 
-    match proc_out.status {
-        ExitStatus(0) | ExitStatus(1) => { } // expected
-        st => {
-            return Err(ProgramError::new(format!("child process exit with {}", st), ProgramErrorKind::Unknown))
+    match proc_out.status.code() {
+        Some(0) | Some(1) => {} // expected
+        Some(st) => {
+            return Err(ProgramError::unknown(format!("child process exit with {}", st)))
+        }
+        None => {
+            return Err(ProgramError::unknown(format!("child process exit with siglan {}", proc_out.status.signal().unwrap())))
         }
     }
 
-    let json = try!(Json::from_reader(&mut MemReader::new(proc_out.output)));
+    let json = try!(Json::from_reader(&mut &proc_out.stdout[..]));
     Ok(try!(Decodable::decode(&mut json::Decoder::new(json))))
 }
 
 fn run() -> ProgramResult<()> {
-    let dir_path = try!(env::current_exe()).dir_path();
-    let mut out = io::stdout();
+    let dir_path = {
+        let mut path = try!(env::current_exe());
+        path.pop();
+        path
+    };
+    let mut out = old_io::stdout();
 
     let mut is_ok = true;
     let mut num_prob = 0;
     let mut total_time = 0;
-    for path in try!(problem_paths(dir_path)) {
+    for path in try!(problem_paths(&dir_path)) {
         let path = path.unwrap();
-        let program = format!("{}", path.filename_display());
+        let program = path.file_name().unwrap().to_string_lossy().to_string();
 
         match run_problem(&path) {
             Ok(ref r) => {
@@ -155,7 +154,7 @@ fn run() -> ProgramResult<()> {
 
 fn main() {
     if let Err(e) = run() {
-        let _ = writeln!(&mut io::stderr(), "{:?}", e);
+        let _ = writeln!(&mut old_io::stderr(), "{:?}", e);
         env::set_exit_status(255);
     }
 }
